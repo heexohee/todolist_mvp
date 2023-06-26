ef592cb957f61c7d8fe75f99e2c6fdc884d33477
heexohee <rsoy2918@naver.com>
2023-06-26 21:32:41 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const users = [
+  
+    {id:1, age:18, name: "강해린"},
+    {id:2, age:20, name: "김민지"},
+    {id:3, age:20, name: "한니"},
+    {id:4, age:19, name: "다니엘"},
+    {id:5, age:16, name: "혜인"},
+    ];
+ 
+  return <div className ='app-style'>
+    <div className='component-style'>{users[0].age}-{users[0].name}</div>
+    <div className='component-style'></div>
+    <div className='component-style'></div>
+    <div className='component-style'></div>
+
+  </div>;
+   
+  
 }
 
 export default App;
