5b4b1621900bab9cb6be9fd3e3ccd724ce849f06
heexohee <rsoy2918@naver.com>
2023-06-27 09:45:55 +0900

    #1 인라인으로 쓰기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
- const users = [
+const App = () => {
+// function App() {
+//  const users = [
   
-    {id:1, age:18, name: "강해린"},
-    {id:2, age:20, name: "김민지"},
-    {id:3, age:20, name: "한니"},
-    {id:4, age:19, name: "다니엘"},
-    {id:5, age:16, name: "혜인"},
-    ];
- 
-  return <div className ='app-style'>
+//     {id:1, age:18, name: "강해린"},
+//     {id:2, age:20, name: "김민지"},
+//     {id:3, age:20, name: "한니"},
+//     {id:4, age:19, name: "다니엘"},
+//     {id:5, age:16, name: "혜인"},
+//     ];
+ const [users, setUsers] = useState([
+  
+      {id:1, age:18, name: "강해린"},
+      {id:2, age:20, name: "김민지"},
+      {id:3, age:20, name: "한니"},
+      {id:4, age:19, name: "다니엘"},
+      {id:5, age:16, name: "혜인"},
+      ]);
+  
+const [name, setName] = useState('');
+const [age, setAge] = useState('');
 
-    {
-      users.map(function(item){
+
+  return (
+    <div>
+      <div> 
+        이름 : &nbsp;
+        <input 
+        value = {name}
+        onChange={(event)=>{
+          setName(event.target.value)
+        }}
+        /> <br />
+        나이 : &nbsp;
+        <input 
+        value = {name}
+        onChange={(event)=>{
+          setAge(event.target.value)
+        }}/>
+     
+      </div>
+  <div className ='app-style'>
+    { users.map(function(item){
         return (
           <div key={item.id} className='component-style'>
       {item.age}-{item.name}
@@ -22,25 +51,10 @@ function App() {
         );
       })
     }
-    {/* <div className='component-style'>
-      {users[0].age}-{users[0].name}
-      </div>
-    <div className='component-style'>
-    {users[1].age}-{users[1].name}
-    </div>
-    <div className='component-style'>
-    {users[2].age}-{users[2].name}
-    </div>
-    <div className='component-style'>
-    {users[3].age}-{users[3].name}
-    </div>
-    <div className='component-style'>
-    {users[4].age}-{users[4].name}
-    </div> */}
 
-  </div>;
-   
-  
+  </div>
+  </div>
+  ); 
 }
 
 export default App;
